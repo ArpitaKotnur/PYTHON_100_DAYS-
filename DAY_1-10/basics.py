9bf622342f5b450e06dfef621b202905944b73e1
Arpita <crazybonnie30@gmail.com>
2026-07-16 14:53:59 +0530

    Create basics.py

---
+++ DAY_1-10/basics.py
@@ -0,0 +1,7 @@
+#comments
+print("hello guys")
+print("hiii my name is \"arpita\" ")
+print("yes guys hehehe why so fun", sep="~",end=":")#sep=seperator and end is end of line
+'''this is
+multiline comment'''
+print(10*10)
