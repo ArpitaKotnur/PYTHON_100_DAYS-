462900d1173b2052fdd8e2359d54fd58fcd4355d
Arpita <crazybonnie30@gmail.com>
2026-07-16 22:58:29 +0530

    Create basic_calculator.py

---
+++ DAY_1-10/basic_calculator.py
@@ -0,0 +1,11 @@
+#calculator
+
+a=int(input("enter first number\n"))
+b=int(input("enter second number\n"))
+print("addition of a and b is", a+b)
+print("subtraction of a and b is",a-b)
+print("multiplication of a and b is",a*b)
+print("divison of a and b is", a/b)
+print("exponential is a^b", a**b)
+print("module of a and b",a%b)
+
