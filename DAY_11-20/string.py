4dc1e65f96bc5ba261e7177ab1debd7061190aeb
Arpita <crazybonnie30@gmail.com>
2026-07-17 13:58:07 +0530

    Create string.py

---
+++ DAY_11-20/string.py
@@ -0,0 +1,25 @@
+name="arpita"
+big_name='''i can 
+write anything
+also giving space
+next line
+this is fun 
+'''
+#multiline  string
+count=0
+print(name)
+print(big_name)
+for char in name:
+    count=count+1
+print("the length of name is\n",count)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+first="hiii"
+second=" beauty"
+print(first+second)#concatenation
+
+
