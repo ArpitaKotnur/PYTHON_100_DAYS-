a42a6ff90c6ccffaf5307e820c2865d0fd6ac903
Arpita <crazybonnie30@gmail.com>
2026-07-18 13:48:43 +0530

    Create loops.py

---
+++ DAY_11-20/loops.py
@@ -0,0 +1,16 @@
+name="arpita"
+for naam in name:
+    print(naam)
+shop=["apple","banana","orange","kiwi"]
+for fruit in shop:
+    print(fruit)
+    for i in fruit:#nested for loop
+        print(i)
+for s in range(5):#prints 5-1 till 4
+    print(s)
+print("-----------------------------------------------")
+for t in range(2,9):#from 2 to 9-1 till 8
+    print(t)
+print("----------------------------------------------")
+for t in range(2,11,2):# 2 = startinng 11=till 10 2= step of 2
+    print(t)
