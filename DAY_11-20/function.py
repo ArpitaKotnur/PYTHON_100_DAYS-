e30c1a92c27985eb83c44aa1f9765d1fa82c39d2
Arpita <crazybonnie30@gmail.com>
2026-07-18 15:01:32 +0530

    Create function.py

---
+++ DAY_11-20/function.py
@@ -0,0 +1,13 @@
+def addition(a,b):
+    print("addition of two  number is",a+b)
+def subtraction(a,b):
+    pass#if you dont want to right uh can simply pass the function but you cant give funtion as blank
+def greater(a,b):
+    if(a<b):
+        print(b ,"is greater")
+    else:
+        print(a, "is greater")
+a=int(input("enter first number\n"))
+b=int(input("enter seconf number\n"))
+addition(a,b)
+greater(a,b)
