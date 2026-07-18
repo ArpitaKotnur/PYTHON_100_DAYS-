1d46f8b0ff91a7ee07d709cedcf0b68324792f16
Arpita <crazybonnie30@gmail.com>
2026-07-18 14:22:08 +0530

    Update loops.py

--- DAY_11-20/loops.py
+++ DAY_11-20/loops.py
@@ -14,3 +14,21 @@ for t in range(2,9):#from 2 to 9-1 till 8
 print("----------------------------------------------")
 for t in range(2,11,2):# 2 = startinng 11=till 10 2= step of 2
     print(t)
+----------------------------------------------------------------------
+i=0
+while(i<5):
+    print(i)
+    i=i+1
+c=int(input("enter the value more than 50\n"))
+while(c>50):
+    c=int(input("enter the value more than 50"))
+    print(c)
+else:
+    print("invalid input") #if while doesn't work then else will
+#python doesn't have do while loop as c nd c++ have
+# create do while in python by making it infinte loop
+while True:
+    c=int(input("enter the value more than 50"))
+    if(c<50):
+        print("invalid input")
+        break
