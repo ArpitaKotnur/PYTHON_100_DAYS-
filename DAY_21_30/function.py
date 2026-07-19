fc5d1ee537a6f128cd3b64db9d0b267526665384
Arpita <crazybonnie30@gmail.com>
2026-07-19 19:08:02 +0530

    Create function.py

---
+++ DAY_21_30/function.py
@@ -0,0 +1,9 @@
+def average(a,b):
+    return (a+b)/2
+print(average(5,5))
+def what(a=2,b=3):#it will replace the default argument with the argument passed
+    return a+b
+print(what(5,5))
+def chat(a,b=3):
+    return a+b
+print(chat(2))#required argument you must give
