fbc5b9bfe5bbbf7b91b141f09064919abafc1dc1
Arpita <crazybonnie30@gmail.com>
2026-07-20 21:50:23 +0530

    Create tuple.py

---
+++ DAY_21_30/tuple.py
@@ -0,0 +1,11 @@
+tup=(1,2,3,4,5,6)
+print(type(tup))
+print(tup[0])
+print(tup[1])
+print(tup[2])
+#tuple is immutable
+tup2=tup[1:4]
+if 56 not in tup:
+    print("no its not")
+else:
+    print("......")
