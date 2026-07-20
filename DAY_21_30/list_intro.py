cec639db2c25a6d4d254ef70f769bf68c7c1d585
Arpita <crazybonnie30@gmail.com>
2026-07-20 12:13:21 +0530

    Update list_intro.py

--- DAY_21_30/list_intro.py
+++ DAY_21_30/list_intro.py
@@ -24,3 +24,31 @@ lis=[i for i in range(20) if i%2==0]
 print(lis)
 #slicing
 print(marks[1:7:2])
+l=[11,45,1,7,5,9,2]
+print(l)
+l.append(10)#appends at end
+print(l)
+l.reverse()#reverse the list
+print(l)
+l.sort()#sort the list in assending order
+print(l)
+l.sort(reverse=True)#sort thr list in dessending order
+print(l)
+
+print(l.index(1))#whats the index of one
+print(l.count(1))#counts the occurence of 1
+m=l
+m[0]=0
+print(l)#m is refernce not new list to make it as new list 
+s=l.copy()
+s[0]=25
+print(l)
+print(s)
+l.insert(3,555)#inserts at index of 3
+#to permanat merge
+p=["wow","shit"]
+l.extend(p)
+print(l)
+#for normal merge
+v=[111,222,333,444,666]
+print(l+v)
